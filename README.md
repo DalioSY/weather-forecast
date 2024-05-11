71a388ed3ddda3470a8c5218fb87068135daaf75
DalioSY <dalioyamada@gmail.com>
2024-05-11 20:13:04 -0300

    weather-forecast

---
+++ README.md
@@ -0,0 +1,47 @@
+<h1 align="center">Front-End CodeBurguer</h1>
+
+<h2>Olá sejam bem-vindo</h2>
+
+<p>☀️Clima para hoje: sua dose de informação atualizada</p>
+
+<p>
+Refiz meu projeto de previsão do tempo, utilizando HTML, CSS, JavaScript e API! Para obter dados em tempo real.
+</p>
+
+<h4>O que há de novo?</h4>
+<ul>
+<li>Design mais intuitivo e bonito: navegue com facilidade e visualize as informações de forma clara e organizada.</li>
+<li>Dados em tempo real: a API integrada garante que você tenha acesso às últimas atualizações do clima, sempre fresquinhas!</li>
+<li>Previsão detalhada para o dia: saiba a temperatura, a sensação térmica, a umidade, a chance de chuva e muito mais, a cada hora do dia.</li>
+</ul>
+
+<p>
+Para ver a previsão do tempo para hoje, acesse o link
+</p>
+
+<h4>Link:</h4>
+
+> Deploy: https://daliosy.github.io/weather-forecast
+
+> Portfólio: https://daliosy.github.io/my-PORTFOLIO
+
+> Linkedin: https://www.linkedin.com/in/dalio-s-yamada/
+
+<p>
+#previsaotempo #desenvolvedorweb #javascript #html #css
+</p>
+<br>
+
+<div id="user-content-toc">
+  <ul align="center">
+    <h2 style="display: inline-block">Imagens:</h2>
+  </ul>
+</div>
+
+<div align="center"> 
+  <img src="./img/git-1.png" width="250px">
+  <img src="./img/git-2.png" width="250px">
+  <img src="./img/git-3.png" width="250px">
+  <img src="./img/git-4.png" width="250px">
+ 
+</div>
---
+++ script.js
@@ -0,0 +1,40 @@
+const searchBtn = document.querySelector(".bx-search-alt-2");
+const input = document.querySelector(".input-city");
+
+async function clickBtn() {
+  const city = document.querySelector(".input-city").value;
+  const apiKey = "cebcd482eda57fa9a6714c1c2ba91885";
+  const apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric&lang=pt_br`;
+
+  const resp = await fetch(apiUrl);
+  const dados = await resp.json();
+  console.log(dados);
+
+  document.querySelector(".country").innerHTML = dados.sys.country;
+  document.querySelector(".city").innerHTML = dados.name;
+  document.querySelector(".temp").innerHTML = `${Math.floor(
+    dados.main.temp
+  )} °C`;
+  document.querySelector(".temp-max").innerHTML = `${Math.floor(
+    dados.main.temp_max
+  )} °C`;
+  document.querySelector(".temp-min").innerHTML = `${Math.floor(
+    dados.main.temp_min
+  )} °C`;
+  document.querySelector(".description").innerHTML =
+    dados.weather[0].description;
+  document.querySelector(".icon").src = `./img/${dados.weather[0].main}.png`;
+  document.querySelector(".humidity").innerHTML = `${dados.main.humidity} %`;
+  document.querySelector(".speed").innerHTML = `${dados.wind.speed} Km/h`;
+
+  display();
+}
+
+input.addEventListener("keydown", clickBtn);
+searchBtn.addEventListener("click", clickBtn);
+
+function display() {
+  const section = document.querySelector(".main");
+
+  section.style.display = "block";
+}
